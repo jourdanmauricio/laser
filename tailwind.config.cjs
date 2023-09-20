cc69cb26a7d6105a0aa6c0e2602ec22771738ef1
mauricio <jourdanmauricio@gmail.com>
2023-09-20 02:29:09 -0300

    .

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['poppins', 'Inter', 'ui-sans-serif', 'sans-serif'],
+        title: ['roboto', 'Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+      },
+    },
+  },
+  plugins: [],
+};
