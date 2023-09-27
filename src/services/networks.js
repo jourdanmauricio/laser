b48fbefce369542012d72b03d362cce64f0b06c8
mauricio <jourdanmauricio@gmail.com>
2023-09-27 00:57:22 -0300

    .

--- src/services/networks.js
+++ src/services/networks.js
@@ -1,19 +1,19 @@
 const BACKEND = import.meta.env.BACKEND;
 const URL = import.meta.env.URL;
 
-export const getOffices = async () => {
-  let offices = [];
+export const getNetworks = async () => {
+  let networks = [];
   try {
-    const res = await fetch(`${BACKEND}/offices`, {
+    const res = await fetch(`${BACKEND}/networks`, {
       headers: { 'Content-Type': 'application/json', url: URL },
     });
     if (res.status !== 200) {
-      console.log('Error GET Offices: ', res.status, res.statusText);
+      console.log('Error GET Netwoks: ', res.status, res.statusText);
     }
-    offices = await res.json();
+    networks = await res.json();
   } catch (error) {
     console.log('ERROR DE CONEXION. Contacte al administrador!!!!', error);
   }
 
-  return offices;
+  return networks[0];
 };
