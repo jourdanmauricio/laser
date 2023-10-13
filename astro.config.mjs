ca1bbeb2d374dabbb50bf39e5f7afbb76a0f9afa
jourdanmauricio <jourdanmauricio@gmail.com>
2023-10-13 12:00:50 -0300

    Change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://jourdanmauricio.github.io',
-  base: '/laser',
+  // site: 'https://jourdanmauricio.github.io',
+  // base: '/laser',
+  site: 'https://laurarodriguez.com.ar',
+  base: '/',
 });
