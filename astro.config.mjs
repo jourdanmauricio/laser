a2901a874db9cba02009b891d296c7c043309926
jourdanmauricio <jourdanmauricio@gmail.com>
2023-10-13 13:42:56 -0300

    Add astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,24 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  experimental: {
+    optimizeHoistedScript: true,
+  },
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.cloudinary.com',
+      },
+    ],
+  },
   // site: 'https://jourdanmauricio.github.io',
   // base: '/laser',
   site: 'https://laurarodriguez.com.ar',
   base: '/',
+  vite: {
+    ssr: {
+      external: ['svgo'],
+    },
+  },
 });
